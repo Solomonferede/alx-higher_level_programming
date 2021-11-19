9cfe3b7ecc431752f55345365647a19903b09b7f
Solomonferede <ezezsolomonferede@gmail.com>
2021-11-19 04:43:01 +0300

    0-add.py file coreccted

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a, b = 1, 2
-sum = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+if __name__ == "__main__":
+    import add_0
+    a, b = 1, 2
+    sum = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
