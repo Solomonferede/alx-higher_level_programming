f1978389c92aa4cd1e476098436bc350022d709f
Solomonferede <ezezsolomonferede@gmail.com>
2021-12-09 10:54:52 +0300

    This module contains a class that defines a square and init method that

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+""" square module:
+
+    define a class Square.
+
+    """
 class Square:
     """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
+
+    methods:
+        __init__: initialize the atribute.
+
     """
 
     def __init__(self, size):
-        """Initializes the data."""
+        """Initializes the data.
+            
+            args:
+                size(int): the size of side of the square
+        """
+
         self.__size = size
