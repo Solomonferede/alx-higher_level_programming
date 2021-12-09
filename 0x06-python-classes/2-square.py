1ec864b512e03e52d895ce4ca475d0dccab8595e
Solomonferede <ezezsolomonferede@gmail.com>
2021-12-09 19:54:23 +0300

    Added a file that define defines a square class with private atribute size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
+"""Square module:
+     that defines a square.
+
+"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
+    """Define a class square
+
+    Methods:
+        __init__: initialize the atributes
+
     """
 
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """Initializing The atribute"""
+
+        try:
+            if not isinstance(size, int):
+                raise TypeError
+            elif size < 0:
+                raise ValueError
+            else:
+                self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
