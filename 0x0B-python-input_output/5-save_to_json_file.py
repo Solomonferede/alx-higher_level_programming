06db2315f000cdf3f21dc84140a455fd0b3af855
Solomonferede <ezezsolomonferede@gmail.com>
2021-12-24 11:06:51 +0300

    A function that Save to joson file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -13,6 +13,5 @@ def save_to_json_file(my_obj, filename):
         filename - name of the file
     """
 
-    json_obj = json.dumps(my_obj)
-    with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(json_obj)
+    with open(filename, mode='w+', encoding='utf-8') as f:
+        json.dump(my_obj, f)
