4320d3c1dcae317743ad862b237c4239ba09624e
Solomonferede <ezezsolomonferede@gmail.com>
2021-12-27 20:47:50 +0300

    Define a class student that initialize value and return the dictionary representation of an instance

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,18 +7,19 @@ of the class
 """
 
 
-def __init__(self, first_name, last_name, age):
-    """Initialize the value of instance atribute when created the instance
+class Student(obj):
+    def __init__(self, first_name, last_name, age):
+        """Initialize the value of instance atribute when created the instance
 
-    args:
-        first_name - The first name of the student
-        last_name - The last name of the student
-        age - Age of the studnt
-    """
+        args:
+            first_name - The first name of the student
+            last_name - The last name of the student
+            age - Age of the studnt
+        """
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+            self.first_name = first_name
+            self.last_name = last_name
+            self.age = age
 
     def to_json(self)
     """define a method which returns the dictionary representation of a class"""
