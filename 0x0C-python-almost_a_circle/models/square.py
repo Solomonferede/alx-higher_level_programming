5d464178462cbf77e8a69084adc1f70f57150662
Solomonferede <ezezsolomonferede@gmail.com>
2021-12-30 16:53:21 +0300

    Update the class Square by adding the public method def update(self, *args, **kwargs) that assigns attributes:

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""the square module define a class Square inherits from
+Rectangle
+"""
+
+
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defining  a Square class inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializing the atribute by calling the super class Rectangle"""
+
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Represent the class object as a string"""
+
+        s = "[Square] ({}) {}/{} - {} ".format(
+                str(self.id), str(self.x),
+                str(self.y), str(self.__width))
+        return s
+
+    @property
+    def size(self):
+        """The width method that.
+        return the value of private attribute __width.
+        """
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Sets the width attribute."""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+            self.__height = value
+
+    def update(self, *args, **kwargs):
+        """Assign an argument to each atribute"""
+
+        arg_len = len(args)
+
+        if arg_len > 0:
+            if arg_len >= 1:
+                self.id = args[0]
+            if arg_len >= 2:
+                self.width = args[1]
+                self.height = args[1]
+            if arg_len >= 3:
+                self.x = args[2]
+            if arg_len >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                if key == 'size':
+                    self.width = value
+                    self.height = value
+                if key == 'x':
+                    self.x = value
+                if key == 'y':
+                    self.y = value
