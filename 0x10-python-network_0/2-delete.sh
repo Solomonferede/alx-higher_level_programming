894c162be798ebfaded465435d4a064e5d227e62
Solomon <ezezsolomonferede@gmail.com>
2024-07-09 11:30:44 +0300

    Send delete requests

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -LS '$1' -X DELETE
+curl -LS "$1" -X DELETE
